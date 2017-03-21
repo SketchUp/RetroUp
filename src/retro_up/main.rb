77108cbac171607424453577ce8a324053eb5db3
Thomas Thomassen <thomas@thomthom.net>
2017-03-21 17:35:37 +0100

    Adding additional audio files for various tools.

--- src/retro_up/main.rb
+++ src/retro_up/main.rb
@@ -4,10 +4,17 @@ module TT::Plugins::RetroUp
 
   unless file_loaded?(__FILE__)
     menu = UI.menu('Plugins')
-    id = menu.add_item('Retro Mode') { self.toggle_retro_mode }
-    menu.set_validation_proc(id)  { self.validation_proc_retro_mode }
+    retro_menu = menu.add_submenu('Retro Mode')
+
+    id = retro_menu.add_item('Activate') { self.toggle_retro_mode }
+    retro_menu.set_validation_proc(id)  { self.validation_proc_retro_mode }
+
+    id = retro_menu.add_item('Debug') { self.toggle_debug_mode }
+    retro_menu.set_validation_proc(id)  { self.validation_proc_debug_mode }
+
     file_loaded(__FILE__)
   end
+
 
   def self.toggle_retro_mode
     self.retro_mode = !self.retro_mode?
@@ -30,23 +37,54 @@ module TT::Plugins::RetroUp
   end
 
 
+  def self.toggle_debug_mode
+    self.debug_mode = !self.debug_mode?
+  end
+
+  def self.debug_mode=(value)
+    Sketchup.write_default('TT_RetroUp', 'DebugMode', value)
+    @debug_mode = value
+  end
+
+  def self.debug_mode?
+    if @debug_mode.nil?
+      @debug_mode = Sketchup.read_default('TT_RetroUp', 'DebugMode', false)
+    end
+    @debug_mode
+  end
+
+  def self.validation_proc_debug_mode
+    self.debug_mode? ? MF_CHECKED : MF_ENABLED
+  end
+
+
   class RetroToolsObserver < Sketchup::ToolsObserver
 
     def onActiveToolChanged(tools, tool_name, tool_id)
-      retro_beep
+      retro_beep(tool_name)
     end
 
     def onToolStateChanged(tools, tool_name, tool_id, tool_state)
-      retro_beep
+      retro_beep(tool_name)
     end
 
     private
 
     RETRO_BEEP_SOUND_FILE = File.join(__dir__, 'beep.wav')
 
-    def retro_beep
+    def retro_beep(tool_name)
       return unless TT::Plugins::RetroUp.retro_mode?
-      UI.play_sound(RETRO_BEEP_SOUND_FILE)
+      debug
+      debug(tool_name)
+      tool_audio_file = File.join(__dir__, 'audio', "#{tool_name}.wav")
+      debug(tool_audio_file)
+      tool_audio_file = RETRO_BEEP_SOUND_FILE unless File.exist?(tool_audio_file)
+      debug(tool_audio_file)
+      UI.play_sound(tool_audio_file)
+    end
+
+    def debug(output = nil)
+      puts output
     end
 
   end
@@ -57,7 +95,7 @@ module TT::Plugins::RetroUp
     def expectsStartupModelNotifications
       return true
     end
-    
+
     def onActivateModel(model)
       observe_model(model)
     end
